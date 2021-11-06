549adc5b0bbe82c48ef1b326628af403c660f66f
DannyChiquillo <87905643+DannyChiquillo@users.noreply.github.com>
2021-11-06 10:34:48 -0600

    Create carro.py

---
+++ carro.py
@@ -0,0 +1,12 @@
+class vehiculo:
+    def encender(self):
+        return "carro encendido" 
+
+
+    def acelerar(self):
+        return "carro en movimiento" 
+
+
+    def frenar(self):
+        return "frenar carro"
+
